14fdc67ce91748c2527c4730a98e99ec95843dff
josePereiro <jose.pereiro.personal@gmail.com>
2024-09-27 10:28:44 -0400

    working version

---
+++ README.md
@@ -0,0 +1,7 @@
+# MassExport
+
+[![CI](https://github.com/josePereiro/MassExport.jl/actions/workflows/CI.yml/badge.svg)](https://github.com/josePereiro/MassExport.jl/actions/workflows/CI.yml)
+<!-- TODO: Make CODECOV work -->
+<!-- [![Coverage](https://codecov.io/gh/josePereiro/MassExport.jl/branch/main/graph/badge.svg)](https://codecov.io/gh/josePereiro/MassExport.jl) -->
+
+
--- src/MassExport.jl
+++ src/MassExport.jl
@@ -1,5 +1,51 @@
 module MassExport
 
-# Write your package code here.
+    function exportall(filter::Function, mod::Module)
+        for sym in names(mod; all = true, imported = true)
+            filter(sym) == true || continue
+            @eval mod export $(sym)
+        end
+    end
 
+    macro exportall_underscore()
+        return quote
+            MetXBase.exportall($(__module__)) do sym
+                startswith(string(sym), r"@?_") && return true
+                return false
+            end
+        end
+    end
+
+    macro exportall_words()
+        return quote
+            MetXBase.exportall($(__module__)) do sym
+                sym == :eval && return false
+                sym == :include && return false
+                startswith(string(sym), r"@?[a-zA-Z]") && return true
+                return false
+            end
+        end
+    end
+
+    macro exportall_non_underscore()
+        return quote
+            MetXBase.exportall($(__module__)) do sym
+                sym == :eval && return false
+                sym == :include && return false
+                startswith(string(sym), r"@?[^_#]") && return true
+                return false
+            end
+        end
+    end
+
+    macro exportall_uppercase()
+        return quote
+            MetXBase.exportall($(__module__)) do sym
+                startswith(string(sym), r"@?[A-Z]") && return true
+                return false
+            end
+        end
+    end
+
+    exportall_words()
 end
